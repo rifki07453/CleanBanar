c6dd498ea15f7ef4a78f4cda6d6f9efa9b75f843
rifki07453 <rifki01462@gmail.com>
2026-04-02 00:16:18 +0800

    chore: update build and project configurations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,18 @@
-pluginManagement {
+﻿pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://repo.eclipse.org/content/repositories/paho-releases/") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
-
-rootProject.name = "CleanBanar"
+rootProject.name = "Myapp"
 include(":app")
- 
