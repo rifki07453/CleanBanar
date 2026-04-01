4dfe24b87c9455a3a2d9d8a312744c26481c7496
rifki07453 <rifki01462@gmail.com>
2026-04-02 00:17:31 +0800

    feat: add core architecture and base classes

---
+++ app/src/main/java/com/example/cleanbanar/core/ui/BaseActivity.kt
@@ -0,0 +1,28 @@
+﻿package com.taupik.myapp.core.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
+    private var _binding: VB? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = inflateBinding(layoutInflater)
+        setContentView(binding.root)
+        setupViews()
+        observeData()
+    }
+
+    abstract fun inflateBinding(layoutInflater: LayoutInflater): VB
+    abstract fun setupViews()
+    open fun observeData() {}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
