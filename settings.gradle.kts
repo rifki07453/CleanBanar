72641cb0872b949dc852078c63bb9d099494ffaf
rifki07453 <rifki01462@gmail.com>
2026-04-14 14:13:52 +0800

    Fix duplicate foojay repository configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,15 +8,7 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
-toolchainManagement {
-    jvm {
-        javaRepositories {
-            repository("foojay") {
-                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
-            }
-        }
-    }
-}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
