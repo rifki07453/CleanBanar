bd4f642513ad90486aa53bfb2e5a78f4b7dd45b0
piecky333 <taupik.rifki@mhs.politala.ac.id>
2026-04-14 13:39:02 +0800

    mau dah nih

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,12 +28,20 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
+    
+    // Explicitly configure Java toolchain
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+
     buildFeatures {
         viewBinding = true
     }
