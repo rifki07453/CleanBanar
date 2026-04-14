3573b75e3b9082462068c4c00a55c651aa1e07f6
rifki07453 <rifki01462@gmail.com>
2026-04-14 14:04:09 +0800

    Fix toolchain build error by updating foojay plugin and explicitly adding toolchainManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,18 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
+            }
+        }
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
