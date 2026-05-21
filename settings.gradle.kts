fa550a07e08f01070da529b18611821383df5fcf
piecky333 <taupik.rifki@mhs.politala.ac.id>
2026-05-21 13:33:37 +0800

    add

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode. set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
